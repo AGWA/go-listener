9d7fb6f8f0ff3c2223a2a5a85204a3a9edaac615
Andrew Ayer <agwa@andrewayer.name>
2020-06-21 14:37:11 -0400

    Add autocert support

---
+++ cert/auto.go
@@ -0,0 +1,36 @@
+package cert
+
+import (
+	"golang.org/x/crypto/acme"
+	"golang.org/x/crypto/acme/autocert"
+	"os"
+	"path/filepath"
+)
+
+func getAutocertCache() autocert.Cache {
+	if cacheDir := os.Getenv("AUTOCERT_CACHE_DIR"); cacheDir != "" {
+		return autocert.DirCache(cacheDir)
+	} else if os.Getuid() == 0 {
+		return autocert.DirCache("/var/lib/autocert-cache")
+	} else if dataDir := os.Getenv("XDG_DATA_HOME"); dataDir != "" {
+		return autocert.DirCache(filepath.Join(dataDir, "autocert-cache"))
+	} else if homeDir, err := os.UserHomeDir(); err == nil {
+		return autocert.DirCache(filepath.Join(homeDir, ".local/share/autocert-cache"))
+	} else {
+		return nil
+	}
+}
+
+func GetCertificateAutomatically(hostnames []string) GetCertificateFunc {
+	manager := &autocert.Manager{
+		Client: &acme.Client{
+			DirectoryURL: os.Getenv("AUTOCERT_ACME_SERVER"),
+		},
+
+		Prompt:     autocert.AcceptTOS,
+		Cache:      getAutocertCache(),
+		HostPolicy: autocert.HostWhitelist(hostnames...),
+		Email:      os.Getenv("AUTOCERT_EMAIL"),
+	}
+	return manager.GetCertificate
+}
